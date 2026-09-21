ac73b0d551bb8540e8090b38f2f7230414e5c71a
yang739002895-debug <yang739002895@gmail.com>
2026-09-21 21:28:16 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wedding-pwa-ui-v2'; // UI 升级版第 2 稿（预览框回退/按钮加长/面板等高/横线跟随字长等）；缓存优先，每次改文件都必须改这个版本号
+const CACHE_NAME = 'wedding-pwa-ui-v3'; // UI 升级版第 3 稿（预览图 364px/按钮收窄 60px）；缓存优先，每次改文件都必须改这个版本号
 const urlsToCache = [
   './index.html',
   './manifest.json',
